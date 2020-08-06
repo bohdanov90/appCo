32ffafd122e7d2cfffbe8f1fe83f234ab12c12b6
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-08-06 09:46:08 +0300

    feat: components and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    StatsComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
