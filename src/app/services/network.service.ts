6b8af895f9790d6d9eea7bbb7c184a8e114fecb6
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-08-06 10:06:08 +0300

    feat: network service logic created

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
+  constructor(
+    private httpClient: HttpClient,
+  ) {}
 
-  constructor() { }
+  public getStats(page: number, limit: number): Observable<any> {
+    return this.httpClient.get(
+      'http://localhost:3001/api/users', { params: new HttpParams()
+        .set('page', page.toString())
+        .set('limit', limit.toString())
+      }
+    );
+  }
+
+  public getCharts(id: number): Observable<any> {
+    return this.httpClient.get(`http://localhost:3001/api/statistics/${id}`);
+  }
 }
